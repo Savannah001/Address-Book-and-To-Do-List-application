36876e7410956c29c5b58c52e129b7cf0262d729
Savannah001 <dansonk899@gmail.com>
2026-05-15 10:45:11 +0300

    Created app.js file which Connects the Address Book and To Do List and business logic to the HTML interface.

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Connects the Address Book and To Do List and business logic to the HTML interface.
+
+//one address book and one to-do list for the entire app
+var myAddressBook = new AddressBook();
+var myToDoList = new ToDoList();
