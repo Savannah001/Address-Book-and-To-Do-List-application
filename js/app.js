0e3951a69850304f60ec238c5f4a7cb57aa57dd0
Savannah001 <dansonk899@gmail.com>
2026-05-15 10:46:21 +0300

    Created Switch between Address Book and To Do List

--- js/app.js
+++ js/app.js
@@ -3,3 +3,24 @@
 //one address book and one to-do list for the entire app
 var myAddressBook = new AddressBook();
 var myToDoList = new ToDoList();
+// Switch between Address Book and To Do List
+function initTabs() {
+  var tabAddress = document.getElementById('tab-address');
+  var tabTodo = document.getElementById('tab-todo');
+  var sectionAddress = document.getElementById('section-address');
+  var sectionTodo = document.getElementById('section-todo');
+  // Address Book tab
+  tabAddress.addEventListener('click', function() {
+    tabAddress.classList.add('active');
+    tabTodo.classList.remove('active');
+    sectionAddress.classList.add('active');
+    sectionTodo.classList.remove('active');
+  });
+  // To Do List tab
+  tabTodo.addEventListener('click', function() {
+    tabTodo.classList.add('active');
+    tabAddress.classList.remove('active');
+    sectionTodo.classList.add('active');
+    sectionAddress.classList.remove('active');
+  });
+}
