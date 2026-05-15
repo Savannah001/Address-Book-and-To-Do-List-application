5f4bb59fffc6f3236e27264f7b4a4bd50eb44746
Savannah001 <dansonk899@gmail.com>
2026-05-15 10:38:05 +0300

    Creating a new service

---
+++ js/to-do.js
@@ -0,0 +1,6 @@
+//The task javascript file
+//Creating new task with a description
+function Task(description) {
+  this.description = description;
+  this.completed = false; // All new tasks start as incomplete
+}
